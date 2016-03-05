c3c7e57e653d18be7cb870ad6299a9b0bc8e9ad7
aryawiratama <stop.drunk@gmail.com>
2016-03-05 17:03:49 +0700

    Add item entity and configure application properties

---
+++ src/main/java/com/arya/latihan/entity/Item.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.arya.latihan.entity;
+
+import java.math.BigDecimal;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+/**
+ *
+ * @author Artha
+ */
+@Entity
+@Table(name = "m_item")
+public class Item {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String id;
+    
+    @Column(name = "code", nullable = false, unique = true)
+    @Size(min = 3, max = 50)
+    @NotNull
+    private String code;
+    
+    @Column(name = "name", nullable = false)
+    @Size(min = 3, max = 255)
+    @NotNull
+    private String name;
+    
+    @Column(name = "price", nullable = false)
+    @NotNull
+    @Min(0)
+    private BigDecimal price;
+    
+    @Column(name = "cost",nullable = false)
+    @NotNull
+    @Min(0)
+    private BigDecimal cost;
+    
+    @Column(name = "stock", nullable = false)
+    @NotNull
+    private BigDecimal stock;
+    
+    @Column(name = "expired_date")
+    private DateTime expiredDate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public BigDecimal getCost() {
+        return cost;
+    }
+
+    public void setCost(BigDecimal cost) {
+        this.cost = cost;
+    }
+
+    public BigDecimal getStock() {
+        return stock;
+    }
+
+    public void setStock(BigDecimal stock) {
+        this.stock = stock;
+    }
+
+    public DateTime getExpiredDate() {
+        return expiredDate;
+    }
+
+    public void setExpiredDate(DateTime expiredDate) {
+        this.expiredDate = expiredDate;
+    }
+    
+}
