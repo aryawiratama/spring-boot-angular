002231c4bd4e36412820d973a5949ae145c6c634
aryawiratama <stop.drunk@gmail.com>
2016-03-07 07:15:09 +0700

    Create form html

---
+++ src/main/java/com/arya/latihan/config/WebConfiguration.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.arya.latihan.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/** 
+ *
+ * @author Artha
+ */
+@Configuration
+public class WebConfiguration extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/item").setViewName("/item/item");
+    }
+    
+}
