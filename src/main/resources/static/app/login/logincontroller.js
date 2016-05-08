289b888c729c86aaa25741e3300388967309ee94
aryawiratama <stop.drunk@gmail.com>
2016-05-08 14:06:39 +0700

    Fix Submit Login menggunakan HTTP BASIC

--- src/main/resources/static/app/login/logincontroller.js
+++ src/main/resources/static/app/login/logincontroller.js
@@ -2,26 +2,17 @@
 var mainApp = angular.module('mainApp.loginController',['mainApp.loginService']);
 
 mainApp.controller('LoginCtrl', ['$scope', '$state', 'LoginService', function($scope, $state, Login){
-    var authenticate = function(credentials){
-        //var headers = credentials ? {authorization : "Basic " + btoa(credentials.username + ":" + credentials.password)} : {};
-        var headers = credentials ? {authorization : "Basic " + btoa(credentials.username + ":" + credentials.password)} : {authorization : "tes"};
-        
-        var user = Login.getUser(headers).get();
-        if(user.name){
-            return true;
-        }else{
-            return false;
-        }
-    };
     
     $scope.login = function(){
-        var authenticated = authenticate($scope.credentials);
-        if(authenticated){
-            $state.go("/");
-            $scope.error = false;
-        }else{
-            $state.go("login");
-            $scope.error = true;
-        }
+        var headers = $scope.credentials ? {authorization : "Basic " + btoa($scope.credentials.username + ":" + $scope.credentials.password)} : {};
+        
+        Login.getUser(headers).get().$promise.then(function(data){
+            if(data.name){
+                $state.go("dashboard");
+                $scope.error = false;
+            }else{
+                $scope.error = true;
+            }
+        });
     };
 }]);
