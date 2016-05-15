82b87c6cb3063f882507ccee7715aaef574ac40d
aryawiratama <stop.drunk@gmail.com>
2016-05-15 19:36:13 +0700

    Add sql file untuk testing

---
+++ src/test/resources/mysql/data-item.sql
@@ -0,0 +1,2 @@
+INSERT INTO m_item (id, code, name, price, cost, stock, expired_date )
+values('test123', 'M-0000000001', 'Kacang', '10000', '8000', '100', '2017-02-01');
---
+++ src/test/resources/mysql/delete-item.sql
@@ -0,0 +1,1 @@
+DELETE FROM m_item;
